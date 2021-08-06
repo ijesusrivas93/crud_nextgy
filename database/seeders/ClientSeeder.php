8ab902e616ef52d89860be426340f0f4f87ab234
jesusrivas93 <rivas.jesus93@gmail.com>
2021-08-05 23:48:46 -0500

    seeder

---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Client;
+
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         Client::truncate();
+
+        $client = new Client;
+    	$client -> name = "Jesús Rivas";
+    	$client -> cc = "1075268384";
+    	$client -> save();
+
+    	$client = new Client;
+    	$client -> name = "David Rivas";
+    	$client -> cc = "1075268954";
+    	$client -> save();
+
+
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        $this->call(ClientSeeder::class);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
